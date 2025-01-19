d62d65628add9207d47e54df975547cb8eefb341
rbalajiramasamy <rbalajibigdata@gmail.com>
2025-01-19 17:01:42 +0530

    First File

---
+++ hello.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+x = ["apple", "banana", "cherry"]
+# for i in x:
+#     #print(i)
+#     if i=='apple':
+#         print('true')
+#         x.append('append')
+#     else:
+#         print("false")
+# Convert to DataFrame
+df = pd.DataFrame(x, columns=['Name'])
+print(df)
+#df.to_excel('df.xlsx',sheet_name='Data',index=False)
+# writer = pd.ExcelWriter("dataframe.xlsx", engine='xlsxwriter')
+# df.to_excel(writer,sheet_name = 'dataframe', index=False)
+
